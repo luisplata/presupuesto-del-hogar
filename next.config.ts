5329d3ea77014aba08c68e66ca02951cb3fbc4f8
Luis Enrique Plata Osorio <www.luisplata@gmail.com>
2025-05-02 14:37:06 +0000

    ++++

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next';
 
+const repoName = 'presupuesto-del-hogar'; // 🚨 Ajusta esto si tu repo es otro (ej: "mi-app")
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   typescript: {
     ignoreBuildErrors: true,
   },
