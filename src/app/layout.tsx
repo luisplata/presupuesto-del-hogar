20905f98a10abeb8c83f51a083f440a5e8abc60c
Luis Enrique Plata Osorio <www.luisplata@gmail.com>
2025-05-02 13:42:05 +0000

    Agregale la posibilidad de crear una Power Web App

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: 'Expense Tracker', // Update title
   description: 'Track your expenses easily.', // Update description
+  manifest: '/manifest.json', // Link to manifest for PWA
 };
 
 export default function RootLayout({
@@ -25,6 +26,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* PWA Meta Tags */}
+        <meta name="application-name" content="Expense Tracker" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Expense Tracker" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#008080" />
+
+        {/* Add to home screen for Safari on iOS */}
+        {/* Use placeholder icons or generate actual icons */}
+        {/* <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" /> */}
+
+        {/* Basic Favicon link */}
+        {/* <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" /> */}
+        {/* <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" /> */}
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
         <Toaster /> {/* Add Toaster here */}
