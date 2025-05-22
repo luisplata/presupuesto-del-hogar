719226cc82ccbfa712fce4dc032df65555139272
luisplata <www.luisplata@gmail.com>
2025-05-22 14:54:54 +0000

    Deploying to gh-pages from @ luisplata/presupuesto-del-hogar@290d95d2fbfde1b816bd908a39d0b823587a199c 🚀

--- _next/static/xZQWXgH5JkFfTjTZa09Q7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/638-89483a1340fd41cf.js","static/chunks/pages/index-c283403bb8f3b1bb.js"],"/_error":["static/chunks/pages/_error-6ae619510b1539d6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
